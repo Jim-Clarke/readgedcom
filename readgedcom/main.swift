54ad3767602a6a338b6bce49d4f9364bd829fa7c
Jim Clarke <clarke@cs.utoronto.ca>
2021-04-28 17:41:03 -0400

    Should be a no-op again; still struggling with getting StringUtilities package to update itself.

--- readgedcom/main.swift
+++ readgedcom/main.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import StringUtilities
 import TextFileUtilities
 
 print("Hello, Genealogical World!")
@@ -20,3 +19,16 @@ try myfile.finalize()
 let scanner = try OptionScanner("ir")
 let usageMessage = "Usage: readgedcom " + scanner.usageString()
 print(usageMessage)
+let nRead = try scanner.getOpts(CommandLine.arguments)
+for o in scanner.allOptions where o.isSet {
+    switch o.optionChar {
+    case "i": print("It's an 'i'!")
+    case "r": print("It's an 'r'!")
+    default:
+        // You won't reach here, because if the arguments refer to an
+        // unrecognized option, then getOpts() has already thrown an
+        // OptionError.failedGet. But Swift wants a default.
+        print(usageMessage)
+        exit(1)
+    }
+}
