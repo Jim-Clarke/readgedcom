eff262a5639c0d33aac0e9df5e6e4f64493aaa72
Jim Clarke <clarke@cs.utoronto.ca>
2021-04-28 17:02:53 -0400

    First commit.

--- readgedcom/main.swift
+++ readgedcom/main.swift
@@ -6,6 +6,17 @@
 //
 
 import Foundation
+import StringUtilities
+import TextFileUtilities
 
-print("Hello, World!")
+print("Hello, Genealogical World!")
 
+let hereDir = "/Users/clarke/Documents/computing/src/Swift/readgedcom/"
+
+let myfile = OutFile(hereDir + "runOutput")
+myfile.writeln("first output to file")
+try myfile.finalize()
+
+let scanner = try OptionScanner("ir")
+let usageMessage = "Usage: readgedcom " + scanner.usageString()
+print(usageMessage)
