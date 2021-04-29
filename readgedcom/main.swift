3901544445a575477fbb4bf8c208995b3ab5c906
Jim Clarke <clarke@cs.utoronto.ca>
2021-04-29 16:31:42 -0400

    First commit after initial package checks succeeded.

--- readgedcom/main.swift
+++ readgedcom/main.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import StringUtilities
 import TextFileUtilities
 
 print("Hello, Genealogical World!")
@@ -13,21 +14,20 @@ print("Hello, Genealogical World!")
 let hereDir = "/Users/clarke/Documents/computing/src/Swift/readgedcom/"
 
 let myfile = OutFile(hereDir + "runOutput")
-myfile.writeln("first output to file")
+let now = Date()
+myfile.writeln("output to file at \(now)")
 try myfile.finalize()
 
 let scanner = try OptionScanner("ir")
 let usageMessage = "Usage: readgedcom " + scanner.usageString()
 print(usageMessage)
 let nRead = try scanner.getOpts(CommandLine.arguments)
+print("args read: \(nRead)")
 for o in scanner.allOptions where o.isSet {
     switch o.optionChar {
     case "i": print("It's an 'i'!")
     case "r": print("It's an 'r'!")
     default:
-        // You won't reach here, because if the arguments refer to an
-        // unrecognized option, then getOpts() has already thrown an
-        // OptionError.failedGet. But Swift wants a default.
         print(usageMessage)
         exit(1)
     }
